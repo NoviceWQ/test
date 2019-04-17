25ba19c94a6b2ec5c384a7a222039dbba8e7fde4
NoviceWQ <45143577+NoviceWQ@users.noreply.github.com>
2019-04-17 15:20:05 +0800

    文件复制
    Java缓冲流文件复制

---
+++ Copy.java
@@ -0,0 +1,19 @@
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Copy {
+    public static void main(String[] args){
+    //字节缓冲输入流
+    BufferedInputStream bis = new BufferedInputStream(new FileInputStream("F:\\File\\123.zip");
+    //字节缓冲输出流
+    BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("F:\\File\\456.zip");
+    int len=0;
+    byte[] bytes = new byte[1024];
+    while((len = bis.read(bytes))!=-1){
+        bos.write(bytes,0,len);  
+  }
+    bos.close();
+    bis.close();
+}
